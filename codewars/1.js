df917c82eb08a11399f11937f94ff0c1d1adc11e
rus55 <r-mit@mail.ru>
2022-11-14 23:53:02 +0300

    new commit

---
+++ codewars/1.js
@@ -0,0 +1,8 @@
+// Your task is to create a function that does four basic mathematical operations.
+// The function should take three arguments - operation(string / char), value1(number), value2(number).
+// The function should return result of numbers after applying the chosen operation.
+
+function basicOp(operation, value1, value2) {
+	return eval(value1 + operation + value2);
+}
+console.log(basicOp('+', 2, 2));
