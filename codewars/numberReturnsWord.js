5aa1dcbd5cf420f40b48dccb38e163027bda409e
rus55 <r-mit@mail.ru>
2022-11-16 00:05:49 +0300

    new commit

---
+++ codewars/numberReturnsWord.js
@@ -0,0 +1,5 @@
+// When provided with a number between 0-9, return it in words.
+
+function switchItUp(number) {
+	return ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'][number];
+}
