913c1b157d225f6ed2cb48c51b84f252cc11880e
rus55 <r-mit@mail.ru>
2022-11-30 23:54:00 +0300

    new commit

---
+++ codewars/stringPrices.js
@@ -0,0 +1,18 @@
+/* You love coffee and want to know what beans you can afford to buy it.
+The first argument to your search function will be a number which represents your budget.
+The second argument will be an array of coffee bean prices.
+Your 'search' function should return the stores that sell coffee within your budget.
+The search function should return a string of prices for the coffees beans you can afford. The prices in this string are to be sorted in ascending order.*/
+
+function search(budget, prices) {
+	prices = prices.sort((a, b) => a - b);
+	let pricesUnderBudget = [];
+	for (let i = 0; i < prices.length; i++) {
+		if (prices[i] <= budget) {
+			pricesUnderBudget.push(prices[i]);
+		}
+	}
+	return pricesUnderBudget.join(',');
+}
+
+console.log(search(3, [6, 1, 2, 9, 2]));
