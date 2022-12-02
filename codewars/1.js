c4fbd29b0e6d4b97278f58251718c675ea4ea345
rus55 <r-mit@mail.ru>
2022-12-03 00:00:53 +0300

    new commit

---
+++ codewars/1.js
@@ -0,0 +1,22 @@
+/*  Your task, is to create NxN multiplication table, of size provided in parameter.
+for example, when given size is 3:
+1 2 3
+2 4 6
+3 6 9
+for given example, the return value should be: [[1,2,3],[2,4,6],[3,6,9]]*/
+
+multiplicationTable = function (size) {
+	let result = [];
+	for (let i = 1; i <= size; i++) {
+		let iteration = [];
+		for (let j = 1; j <= size; j++) {
+			iteration.push(i * j);
+		}
+		result.push(iteration);
+	}
+	return result;
+}
+
+console.log(multiplicationTable(3));
+
+
